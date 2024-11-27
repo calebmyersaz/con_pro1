73e099073031da212d1b62420d4ba713c8a60f8c
DillonMUMDGithub <159959205+DillonMUMDGithub@users.noreply.github.com>
2024-11-27 01:39:07 -0500

    Making more progess towards a plot

--- controls_pro1.py
+++ controls_pro1.py
@@ -1,7 +1,32 @@
 import sympy as sp
 import numpy as np
 
+def Calc_M(A_f,B_f,C_f,N_m,N_e):
+    np_A = np.array(A_f).astype(np.float64)
+    np_B = np.array(B_f).astype(np.float64)
+    np_C = np.array(C_f).astype(np.float64)
+    np_Nm = np.array(N_m).astype(np.float64)
+    np_Ne = np.array(N_e).astype(np.float64)
 
+    def matrix_eq(M_flat):
+        M = M_flat.reshape(4, 4)  # Convert the flat array back to a matrix
+        term1 = np.dot(np_A, M) + np.dot(M, np_A.T)
+        term2 = np.dot(M, np.dot(np_C.T, np.dot(np.linalg.inv(np_Nm), np.dot(np_C, M))))
+        term3 = np.dot(np_B, np.dot(np_Ne, np_B.T))
+        return (term1 + term2 - term3).flatten()  # Flatten the result for numerical solving
+
+    # Initial guess for the matrix M (a 16-element vector)
+    initial_guess = np.eye(4).flatten()
+
+    # Solve using fsolve to minimize the residual of the equation
+    from scipy.optimize import fsolve
+    M_solution_flat = fsolve(matrix_eq, initial_guess)
+
+    # Convert the solution back to a 4x4 matrix
+    M_solution = M_solution_flat.reshape(4, 4)
+
+    # print(M_solution)
+    return M_solution
 
 
 
@@ -57,6 +82,8 @@ A = sp.Matrix([[0,0,1,0],
 v,v_1,v_2 = sp.symbols('v v_1 v_2')
 v = sp.Matrix([[v_1],[v_2]])
 
+
+
 # Equation 9
 ctrlp1 = J * sp.Matrix([[1/(2*l*sp.sin(theta_2/2)),1],
                         [-1/(2*l*sp.sin(theta_2/2)),1]]) * v
@@ -96,22 +123,27 @@ n = sp.Matrix([[1],[1]])
 
 # n_e definition
 n_e = -K1.inv()*n
-
+# sp.pprint(n_e)
 
 
 
 
 print('n_e def')
-sp.pprint(n_e)
+# sp.pprint(n_e)
 
 A_f = sp.Matrix([[0,0,1,0],
                  [0,0,0,1],
                  [0,0,0,0],
                  [0,0,0,0]])
 
-B_f = sp.Matrix([[0],[0],[1],[1]])
+B_f = sp.Matrix([[0, 0],
+                 [0, 0],
+                 [0, 1],
+                 [1, 0]])
 
-C_f = sp.Matrix([[1, 1, 0, 0]])
+C_f = sp.Matrix([[0,1,0,0],
+               
+                [1,0,0,0]])
 
 # Equation 15
 Kal_thdot = sp.Matrix([[0,0,1,0],
@@ -121,16 +153,52 @@ Kal_thdot = sp.Matrix([[0,0,1,0],
                                                          [0,0],
                                                          [0,1],
                                                          [1,0]])*(accel+n_e)
-sp.pprint(Kal_thdot)
+# sp.pprint(Kal_thdot)
 
-n_m = sp.Matrix([[1],[1]])
+n_m = sp.Matrix([[1],[-1]])
 
 # Equation 16
 Kal_y = sp.Matrix([[1, 0, 0, 0],
                    [0, 1, 0, 0]])*Kal_th + n_m
 
 th12_e, th1_e, th2_e = sp.symbols('th12e th1e th2e')
+# th12_edot, th1_edot, th2_edot = sp.symbols('th12edot th1edot th2edot')
+
+F,Kal_th_e,Kal_th_e_dot, a = sp.symbols('F Kal_th_e Kal_th_e_dot, a')
 th12_edot, th1_edot, th2_edot = sp.symbols('th12edot th1edot th2edot')
+
+Kal_th_e = sp.Matrix([[th1_e],
+                      [th12_e],
+                      [th1_edot],
+                      [th12_edot]])
+a = accel
+F = sp.Matrix([[1,0],
+               [0,1],
+               [1,0],
+               [0,1]])
+# Kal_th_e_dot = A_f*Kal_th_e + B_f*a + F*(Kal_y - C_f*Kal_th_e)
+# G = (Kal_y - C_f*Kal_th_e)
+
+# sp.pprint(Kal_th_e_dot)
+# np_matrix = np.array(n_e).astype(np.float64)
+# N_cov = np.cov(np_matrix.T)
+N_e = sp.Matrix([[1, -.5],[-1, 2]])
+
+N_m = sp.Matrix([[1, -.33],[-1, .75]])
+# M = sp.MatrixSymbol('M',4,4)
+M_soln = Calc_M(A_f,B_f,C_f,N_m,N_e)
+M=M_soln
+eq = A_f*M + M* A_f.T - M*C_f.T*N_m.inv()*C_f*M + B_f*N_e*B_f.T
+
+# soln = sp.linsolve(eq,M)
+# sp.pprint(eq)
+F = M*C_f.T*N_m.inv()
+
+Kal_th_e_dot = A_f*Kal_th_e + B_f*a + F*(Kal_y - C_f*Kal_th_e)
+
+# sp.pprint(Kal_th_e_dot)
+# print(M_soln)
+# Equation 20
 x_est = sp.Matrix([[2*l*sp.cos(0.5*th2_e)],
                    [0.5*(th1_e + th12_e)],
                    [-l*th2_edot*sp.sin(0.5*th2_e)],
@@ -153,26 +221,63 @@ x_est = sp.Matrix([[2*l*sp.cos(0.5*th2_e)],
 # NUMERICAL
 # Establish Initial Conditions:
 
-# m1_num =1
-# m2_num =2
-# l_num = 1
-# I1_num = 0.5
-# I2_num = 0.5
-# # sp.pprint(J)
-# J = J.subs({I_1:I1_num,I_2:I2_num,l: l_num, m_1:m1_num,m_2:m2_num})
-# w = w.subs({I_1:I1_num,I_2:I2_num,l: l_num, m_1:m1_num,m_2:m2_num})
-# B = B.subs({I_1:I1_num,I_2:I2_num,l: l_num, m_1:m1_num,m_2:m2_num})
+m1_num =1
+m2_num =2
+l_num = 1
+l20_num = 1
+I1_num = 0.5
+I2_num = 0.5
+theta1 = np.pi/4
+theta2 = np.pi/4
+theta12 = np.pi/2
+theta12d = 0.157
+theta1d = 0.157
+theta2d = 0.157
+# sp.pprint(J)
+J = J.subs({I_1:I1_num,I_2:I2_num,l: l_num, m_1:m1_num,m_2:m2_num,theta_2: theta2,theta_12dot:theta12d,theta_1dot:theta1d})
+w = w.subs({I_1:I1_num,I_2:I2_num,l: l_num, m_1:m1_num,m_2:m2_num,theta_2: theta2,theta_12dot:theta12d,theta_1dot:theta1d})
+B = B.subs({I_1:I1_num,I_2:I2_num,l: l_num, m_1:m1_num,m_2:m2_num,theta_2: theta2,theta_12dot:theta12d,theta_1dot:theta1d})
 
-# # sp.pprint(J)
-# # sp.pprint(w)
-# # sp.pprint(B)
-# # sp.pprint(x)
+sp.pprint(J)
+sp.pprint(w)
+sp.pprint(B)
+sp.pprint(x)
 
-# u_ctrl = u_ctrl.subs({I_1:I1_num,I_2:I2_num,l: l_num, m_1:m1_num,m_2:m2_num})
-# # sp.pprint(u_ctrl)
+# eq 10, still need v inputs
+u_ctrl = u_ctrl.subs({I_1:I1_num,I_2:I2_num,l: l_num, m_1:m1_num,m_2:m2_num,theta_2: theta2,theta_12dot:theta12d,theta_1dot:theta1d,theta_2dot: theta2d})
+sp.pprint(u_ctrl)
 
-# tau_m = u_ctrl[0]
-# tau_b = u_ctrl[1]
+tau_m = u_ctrl[0]
+tau_b = u_ctrl[1]
 
-# # tau_m = tau_m.subs({theta_2:0.5})
-# # sp.pprint(tau_m)
+d = d.subs({I_1:I1_num,I_2:I2_num,l: l_num, m_1:m1_num,m_2:m2_num,theta_2: theta2,theta_12dot:theta12d,theta_1dot:theta1d,theta_2dot: theta2d})
+sp.pprint(d)
+
+# eq 14 Still needs linear acceleration vals
+accel = accel.subs({I_1:I1_num,I_2:I2_num,l: l_num,l_20:l20_num, m_1:m1_num,m_2:m2_num,theta_2: theta2,theta_12dot:theta12d,theta_1dot:theta1d,theta_2dot: theta2d})
+
+
+# est_th1 = 0.5
+# est_th12 = 0.5
+est_th1d = 0.5
+est_th12d = 0.5
+ax = 0
+ay = 0
+
+Kalman_y = Kal_y.subs({theta_1: theta1, theta_12:theta12})
+est_th1 = Kalman_y[0]
+est_th12 = Kalman_y[1]
+est_th2 = est_th12 - est_th1
+est_th2d = est_th12d - est_th1d
+
+Kalman_Thdot = Kal_th_e_dot.subs({I_1:I1_num,I_2:I2_num,l: l_num,l_20:l20_num, m_1:m1_num,m_2:m2_num,theta_2: theta2,theta_12dot:theta12d,theta_1dot:theta1d,
+                             theta_2dot: theta2d, theta_1:theta1, theta_12: theta12, th1_e:est_th1,th12_e:est_th12,th1_edot:est_th1d,th12_edot:est_th12d, a_x:ax,a_y:ay})
+
+estimated_x = x_est.subs({th1_e:est_th1,th12_e:est_th12,th2_e:est_th2,l:l_num,th12_edot:est_th12d,th1_edot:est_th1d,th2_edot:est_th2d})
+
+K_f = sp.Matrix([[10,10,10,10],
+                 [5,5,5,5]])
+x_d = sp.Matrix([[0],[0],[0],[0]])
+v = K_f*(x_d - estimated_x)
+
+# sp.pprint(Kalman_Thdot)
--- sympytest.py
+++ sympytest.py
@@ -1,73 +1,26 @@
-from sympy import symbols, Matrix, eye, simplify, pprint
 
-# Define symbols for state variables, input, measurement, noise covariances
-x1, x2 = symbols('x1 x2')  # State vector components
-u = symbols('u')           # Control input
-z = symbols('z')           # Measurement
-x_pred1, x_pred2 = symbols('x_pred1 x_pred2')  # Predicted states
+from sympy import symbols, Matrix, Transpose, Inverse, Eq, simplify, MatrixSymbol, solve
+n = 4
+m = 2
+p = 2
+# Define matrix symbols
+Af = MatrixSymbol('Af', n, n)  # n x n matrix
+M = MatrixSymbol('M', n, n)   # n x n matrix
+Cf = MatrixSymbol('Cf', m, n) # m x n matrix
+Nm = MatrixSymbol('Nm', m, m) # m x m matrix
+Bf = MatrixSymbol('Bf', n, p) # n x p matrix
+Ne = MatrixSymbol('Ne', p, p) # p x p matrix
 
-# State vector (x) and predicted state vector (x_pred)
-x = Matrix([x1, x2])
-x_pred = Matrix([x_pred1, x_pred2])
+# Define the equation
+eq = Eq(Af * M + M * Transpose(Af) - M * Transpose(Cf) * Inverse(Nm) * Cf * M + Bf * Ne * Transpose(Bf), 0)
 
-# State-space matrices (symbolic)
-A = Matrix([[1, 1], [0, 1]])  # Example state transition matrix
-B = Matrix([[0.5], [1]])      # Control input matrix
-C = Matrix([[1, 0]])          # Measurement matrix
-Q = eye(2) * 0.1              # Process noise covariance (symbolic)
-R = eye(1) * 1                # Measurement noise covariance (symbolic)
+# Try to manipulate the equation
+# Rearrange it to group terms involving M:
+eq1 = Eq(Af * M + M * Transpose(Af) - M * Transpose(Cf) * Inverse(Nm) * Cf * M, -Bf * Ne * Transpose(Bf))
 
-# Initial state estimate and covariance
-x_est = Matrix([0, 0])        # Initial state estimate
-P = eye(2)                    # Initial error covariance
+# Try solving symbolically for M (this will likely not work directly due to nonlinearity)
+from sympy import solve
+solution = solve(eq1, M)
 
-# Prediction equations
-x_pred = A * x_est + B * u
-P_pred = A * P * A.T + Q
-
-# Display results
-print("Predicted State (x_pred):")
-pprint(x_pred)
-print("\nPredicted Covariance (P_pred):")
-pprint(P_pred)
-
-# Compute Kalman gain
-S = C * P_pred * C.T + R  # Innovation covariance
-K = P_pred * C.T * S.inv()  # Kalman gain
-
-# Update state estimate
-y = z - C * x_pred  # Measurement residual (innovation)
-x_est = x_pred + K * y
-
-# Update error covariance
-P = (eye(P_pred.shape[0]) - K * C) * P_pred
-
-# Display results
-print("\nKalman Gain (K):")
-pprint(K)
-print("\nUpdated State Estimate (x_est):")
-pprint(x_est)
-print("\nUpdated Covariance (P):")
-pprint(P)
-
-
-# Symbolic iteration for one measurement
-u_value = 1  # Example input value
-z_value = 2  # Example measurement value
-
-# Substitute values into prediction step
-x_pred_eval = x_pred.subs(u, u_value)
-P_pred_eval = P_pred
-
-# Substitute values into update step
-y_eval = z_value - C * x_pred_eval
-K_eval = simplify(P_pred_eval * C.T * (C * P_pred_eval * C.T + R).inv())
-x_est_eval = simplify(x_pred_eval + K_eval * y_eval)
-P_eval = simplify((eye(P_pred_eval.shape[0]) - K_eval * C) * P_pred_eval)
-
-print("\nPredicted State with Substituted Values (x_pred):")
-pprint(x_pred_eval)
-print("\nUpdated State Estimate with Substituted Values (x_est):")
-pprint(x_est_eval)
-print("\nUpdated Covariance with Substituted Values (P):")
-pprint(P_eval)
+# Print the solution
+print(solution)
